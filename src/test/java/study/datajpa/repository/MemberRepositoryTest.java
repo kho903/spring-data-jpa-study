61b23efcc11ebff06a08e5378fca7562ba151a1e
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-12 15:25:47 +0900

    feat: MemberRepository Using Spring Data JPA, MemberRepositoryTest

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package study.datajpa.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import study.datajpa.entity.Member;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+	@Autowired
+	MemberRepository memberRepository;
+
+	@Test
+	public void testMember()  {
+		Member member = new Member("memberA");
+		Member savedMember = memberRepository.save(member);
+
+		Optional<Member> optionalFindMember = memberRepository.findById(savedMember.getId());
+		Member findMember = optionalFindMember.get();
+
+		assertThat(findMember.getId()).isEqualTo(member.getId());
+		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+		assertThat(findMember).isEqualTo(member);
+	}
+}
