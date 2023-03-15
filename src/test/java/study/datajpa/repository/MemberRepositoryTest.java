0280c08e429afd9390bc885096d04ed3aff27c05
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-15 11:53:44 +0900

    feat: Spring Data JPA 에서의 fetch join
    - MemberRepository
    	- @Query jpql
    	- findAll override 후 @EntityGraph
    	- @EntityGraph + @Query
    - NamedEntityGraph in Member Entity

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -251,4 +251,33 @@ class MemberRepositoryTest {
 		// then
 		assertThat(resultCount).isEqualTo(3);
 	}
+
+	@Test
+	void findMemberLazy() {
+		// given
+		// member1 -> teamA
+		// member2 -> teamB
+
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		teamRepository.save(teamA);
+		teamRepository.save(teamB);
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 10, teamB);
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		em.flush();
+		em.clear();
+
+		// when
+		// List<Member> members = memberRepository.findAll(); // N + 1 -> 오버라이드 후 @EntityGraph로 해결 (페치 조인)
+		// List<Member> members = memberRepository.findMemberFetchJoin();
+		List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+		for (Member member : members) {
+			System.out.println("member.username = " + member.getUsername());
+			System.out.println("member.team.class = " + member.getTeam().getClass());
+			System.out.println("member.team = " + member.getTeam().getName());
+		}
+	}
 }
