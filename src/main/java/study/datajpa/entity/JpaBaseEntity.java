5c67f89ad743caefc79a4505b94ee2e91e3d8b4b
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-20 14:16:09 +0900

    feat: add JpaBaseEntity in Member, Team Entity using 순수 JPA Base Entity

---
+++ src/main/java/study/datajpa/entity/JpaBaseEntity.java
@@ -0,0 +1,30 @@
+package study.datajpa.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public class JpaBaseEntity {
+
+	@Column(updatable = false)
+	private LocalDateTime createdDate;
+	private LocalDateTime updatedDate;
+
+	@PrePersist
+	public void prePersist() {
+		LocalDateTime now = LocalDateTime.now();
+		createdDate = now;
+		updatedDate = now;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedDate = LocalDateTime.now();
+	}
+}
