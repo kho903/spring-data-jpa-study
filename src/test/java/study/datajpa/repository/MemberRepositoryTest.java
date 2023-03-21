2e4a44287639bec9db9971a82082defac17b9e06
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-21 16:04:24 +0900

    feat: Specifications (명세)
    - MemberSpec
    - MemberRepository implements MemberSpec
    - specBasic() in test

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,12 +6,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -328,5 +330,26 @@ class MemberRepositoryTest {
 	@Test
 	void callCustom() {
 		List<Member> result = memberRepository.findMemberCustom();
+	}
+
+	@Test
+	void specBasic() {
+		// given
+		Team teamA = new Team("teamA");
+		em.persist(teamA);
+
+		Member m1 = new Member("m1", 0, teamA);
+		Member m2 = new Member("m2", 0, teamA);
+		em.persist(m1);
+		em.persist(m2);
+
+		em.flush();
+		em.clear();
+
+		// when
+		Specification<Member> spec = MemberSpec.username("m1").and(MemberSpec.teamName("teamA"));
+		List<Member> result = memberRepository.findAll(spec);
+
+		Assertions.assertThat(result.size()).isEqualTo(1);
 	}
 }
