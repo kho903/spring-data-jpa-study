8110076abf85c9c194c50b680b353cc0abef7b53
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-21 15:32:32 +0900

    feat: implements Persistable in Item Entity

--- src/main/java/study/datajpa/entity/Item.java
+++ src/main/java/study/datajpa/entity/Item.java
@@ -1,23 +1,41 @@
 package study.datajpa.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Getter
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Item {
+public class Item implements Persistable<String> {
 
 	@Id
 	// @GeneratedValue
 	// private Long id;
 	private String id;
 
+	@CreatedDate
+	private LocalDateTime createdDate;
+
 	public Item(String id) {
 		this.id = id;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public boolean isNew() {
+		return createdDate == null;
 	}
 }
